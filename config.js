76eb5cd02d896b7478bb5da650c35cd37c7b1a05
Michael Kargl <michael.kargl@posteo.net>
2026-04-05 06:27:55 +0200

    feat: adds github pages deployment

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/osasoftworks/portfolio/osaportfolio/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/portfolio-photo/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'OSA | Michael Kargl',
   subHeading: '',
   // social
